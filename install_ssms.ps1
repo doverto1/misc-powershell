4a4a9907e1e7c5725deab819fd8c38e85bf92092
Don Overton <donald.overton@asu.edu>
2023-08-25 14:09:17 -0700

    Intial Commit

---
+++ install_ssms.ps1
@@ -0,0 +1,11 @@
+﻿# Define the download URL for the SSMS installer
+$ssmsDownloadUrl = "https://aka.ms/ssmsfullsetup"
+
+# Define the path where the installer will be downloaded
+$installerPath = "C:\Users\$env:USERNAME\Desktop\SSMSInstaller.exe"
+
+# Download the SSMS installer
+Invoke-WebRequest -Uri $ssmsDownloadUrl -OutFile $installerPath
+
+# Install SSMS
+Start-Process -FilePath $installerPath -ArgumentList "/install /quiet /norestart"
